143f0b035e14b727d58c95e268e83303ea1027ef
David <AquilaVirtual>
2018-09-12 09:30:50 -0700

    worked on search functionality, able to console.log searched item

--- true-business/src/App.js
+++ true-business/src/App.js
@@ -3,14 +3,27 @@ import { Route } from 'react-router-dom';
 import LandingPage from './components//LandingPage';
 import SignUp from './components//SignUp';
 import SignIn from './components//SignIn';
+import NavBar from  './components//NavBar';
+import Search from './components/Search'
 import './css/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    }
+
+  }
+ SearchBusiness = () => {
+ 
+ }
   render() {
     return (
       <div className="app-container">
         <Route path="/" component={LandingPage} exact />
         <Route path="/signup" component={SignUp} />
+        <Route path="/search" component={Search} />   
         <Route path="/signin" component={SignIn} />
       </div>
     );
--- true-business/src/components/NavBar.js
+++ true-business/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Popover, PopoverBody, PopoverHeader } from 'reactstrap';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Route } from 'react-router-dom';
+import Search from './Search';
 
 import logo from '../imgs/logo.png';
 
@@ -9,21 +10,40 @@ import '../css/NavBar.css';
 class NavBar extends Component {
   state = {
     popoverOpen: false,
+    search: ''
   };
 
   toggle = () => {
     this.setState({ popoverOpen: !this.state.popoverOpen });
   };
 
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  searchWord = (event) => {
+   console.log("Fire",this.state.search);
+    event.preventDefault();
+    const newSearch = {
+        search: this.state.search
+    };
+
+    // this.props.search(newSearch);
+    //     this.setState({
+    //         search: '',
+
+    //     });
+  }
   render() {
     return (
       <div className="navbar-container">
         <img src={logo} className="navbar-container__logo" />
         <div className="navbar-container__center">
-          <input onClick={this.toggle} id="signInPop" style={{ width: '200px' }} className="navbar-container__input" />
+          <input onClick={this.toggle} id="signInPop" style={{ width: '200px' }} 
+          className="navbar-container__input" placeholder="search" type="text" name="search" value={this.state.search} onChange={this.handleInputChange}  />
           <div className="navbar-container__buttons">
             <button className="navbar-container__button">Review </button>
-            <button className="navbar-container__button">Search </button>
+            <button className="navbar-container__button" onClick={this.searchWord} >Search </button>
           </div>
           <Popover
             placement="bottom"
@@ -49,11 +69,12 @@ class NavBar extends Component {
           <div
             className="navbar-container__sign"
             onClick={() => {
-              this.props.history.push(`/signin`);
+              this.props.history.push(`/signin`); 
             }}>
             Sign In
           </div>
         </div>
+         <Search  state={this.state}/> 
       </div>
     );
   }
