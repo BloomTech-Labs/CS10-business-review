ff60b3376abddfac9ff732538dc27d2219b5c419
Jason Metten <metten@Jasons-MacBook-Air.local>
2018-09-13 13:37:03 -0500

    Changes to stop local builds from crashing due to passport

--- true-business-backend/controllers/passportController.js
+++ true-business-backend/controllers/passportController.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.googleClientID,
-      clientSecret: process.env.googleClientSecret,
+      clientID: process.env.googleClientID || keys.googleAuthClientID,
+      clientSecret: process.env.googleClientSecret || keys.googleAuthSecret,
       callbackURL: '/auth/google/callback'
     },
     function(accessToken, refreshToken, profile, done) {
