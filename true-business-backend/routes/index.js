9507435abb51ecdf2487d9a9ef8cbdbf5c1514bd
Jason Metten <metten@Jasons-MacBook-Air.local>
2018-09-13 09:32:14 -0500

    Added persistence

--- true-business-backend/routes/index.js
+++ true-business-backend/routes/index.js
@@ -32,6 +32,14 @@ router.get('/', (request, response) => {
   response.status(200).json({ api: 'Server running OK.' });
 });
 
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
 router.post('/register', (request, response) => {
   UserController.register(request, response);
 });
