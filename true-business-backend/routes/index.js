b75a42bbf347ac9bb9203a49780fccb33727bc44
Jason Metten <metten@Jasons-MacBook-Air.local>
2018-09-13 11:05:40 -0500

    Refactored to use passportController.js

--- true-business-backend/routes/index.js
+++ true-business-backend/routes/index.js
@@ -1,48 +1,22 @@
 const express = require('express');
 const server = require('../server');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 
 const UserController = require('../controllers/userController');
 const BusinessController = require('../controllers/businessController');
+const PassportController = require('../controllers/passportController');
 const router = express.Router();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleAuthClientID,
-      clientSecret: keys.googleAuthSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    function(accessToken, refreshToken, profile, done) {
-      console.log(accessToken);
-      console.log(accessToken);
-      console.log(accessToken);
-    }
-  )
-);
-
-router.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-router.get('/auth/google/callback', passport.authenticate('google'));
 
 router.get('/', (request, response) => {
   response.status(200).json({ api: 'Server running OK.' });
 });
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
+// passport.serializeUser(function(user, cb) {
+//   cb(null, user);
+// });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+// passport.deserializeUser(function(obj, cb) {
+//   cb(null, obj);
+// });
 
 router.post('/register', (request, response) => {
   UserController.register(request, response);
@@ -63,6 +37,18 @@ router.get('/api/business/ByName/:name', function(request, response) {
 router.get('/api/business/:id', function(req, res) {
   BusinessController.getBusinessById(req, res);
 });
+
+router.get(
+  '/auth/google',
+  PassportController.passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+router.get(
+  '/auth/google/callback',
+  PassportController.passport.authenticate('google')
+);
 
 // router.get("/API/Business", function(req, res) {
 //     Business.find({}, function(err, allbusiness))
