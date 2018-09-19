1aca0f59c272df394cf3dac47f821431c630711a
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-09-19 14:10:18 -0700

    Started Stripe
    404 Error happening, but the groundwork is there.

--- true-business-backend/routes/index.js
+++ true-business-backend/routes/index.js
@@ -62,4 +62,19 @@ router.get('/api/business/', function(req, res) {
   BusinessController.getAllBusiness(req, res);
 });
 
+router.post('/charge', async (req, res) => {
+  try {
+    let { status } = await stripe.charges.create({
+      amount: 2000,
+      currency: 'usd',
+      description: 'An example charge',
+      source: req.body,
+    });
+
+    res.json({ status });
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
 module.exports = router;
