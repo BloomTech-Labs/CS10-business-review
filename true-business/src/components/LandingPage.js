b16950b450b985f0c29fa79053214c63ca387eb7
Jason Metten <metten0@gmail.com>
2018-09-27 14:50:12 -0500

    No longer broken...and I helped

--- true-business/src/components/LandingPage.js
+++ true-business/src/components/LandingPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Modal from "react-modal";
 import BusinessThumbnail from "./BusinessThumbnail";
+import StarRatings from "react-star-ratings";
+
 import "../css/LandingPage.css";
 import "../css/GeneralStyles.css";
-import Dropzone from "react-dropzone";
-import axios from "axios";
 
 import NavBar from "./NavBar";
 
@@ -39,7 +39,7 @@ class LandingPage extends Component {
     this.closeModal = this.closeModal.bind(this);
   }
 
-  openModal(info, event) {
+  openModal(event, info) {
     this.setState({ modalIsOpen: true, modalInfo: info });
   }
 
@@ -51,35 +51,6 @@ class LandingPage extends Component {
     window.scrollTo(0, 0);
   };
 
-  handleDrop = files => {
-    const uploaders = files.map(file => {
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("tags", ``);
-      formData.append("upload_preset", "true-business"); // Replace the preset name with your own
-      formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY); // Replace API key with your own Cloudinary key
-      formData.append("timestamp", (Date.now() / 1000) | 0);
-
-      // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
-      return axios
-        .post(
-          "https://api.cloudinary.com/v1_1/ddhamypia/image/upload",
-          formData,
-          {
-            headers: { "X-Requested-With": "XMLHttpRequest" }
-          }
-        )
-        .then(response => {
-          const data = response.data;
-          const fileURL = data.secure_url; // You should store this URL for future references in your app
-          console.log(data);
-        });
-    });
-  };
-  // axios.all(uploaders) => {
-  //   // ... perform after upload is successful operation
-  // });
-
   render() {
     return (
       <div>
@@ -88,59 +59,76 @@ class LandingPage extends Component {
           <div className="landing-container__reviews-container">
             <div className="landing-container__title">Popular Reviews</div>
             <div className="landing-container__reviews">
-              {this.props.businesses.map(business => {
-                return (
-                  <div
-                    key={business._id}
-                    onClick={() => this.openModal(this, business)}
-                  >
-                    <BusinessThumbnail business={business} key={business._id} />
-                  </div>
-                );
+              {this.props.reviews.map((review, i) => {
+                if (i < 5) {
+                  return (
+                    // Need to write a component that shows all the reviews by a certain user
+                    // <div key={review._id} onClick={() => this.props.userReviews(user)}>
+                    <div key={review._id}>
+                      <div className="landing-container__review">
+                        <div className="landing-container__item">
+                          {review.newMongoId.name}
+                        </div>
+                        <div
+                          className="landing-container__picture"
+                          onClick={() => this.openModal(this, review)}
+                        />
+                        <ul className="landing-container__item--hover">
+                          @{review.reviewer.username}
+                        </ul>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
               })}
             </div>
           </div>
           <div className="landing-container__reviews-container">
             <div className="landing-container__title">Popular Businesses</div>
             <div className="landing-container__reviews">
-              {this.props.businesses.map(business => {
-                return (
-                  <div
-                    key={business._id}
-                    onClick={() => this.props.getBusiness(business, true)}
-                  >
-                    <BusinessThumbnail business={business} key={business._id} />
-                  </div>
-                );
+              {this.props.businesses.map((business, i) => {
+                if (i < 5) {
+                  return (
+                    <div
+                      key={business._id}
+                      onClick={() => this.props.getBusiness(business, true)}
+                    >
+                      <BusinessThumbnail
+                        business={business}
+                        key={business._id}
+                      />
+                    </div>
+                  );
+                }
+                return null;
               })}
             </div>
           </div>
           <div className="landing-container__reviews-container">
             <div className="landing-container__title">Popular Reviewers</div>
             <div className="landing-container__reviews">
-              <div className="landing-container__review">
-                <div className="landing-container__picture" />
-                <ul className="landing-container__item--hover">@Reviewer</ul>
-              </div>
-              <div className="landing-container__review">
-                <div className="landing-container__picture" />
-                <ul className="landing-container__item--hover">@Reviewer</ul>
-              </div>
-              <div className="landing-container__review">
-                <div className="landing-container__picture" />
-                <ul className="landing-container__item--hover">@Reviewer</ul>
-              </div>
-              <div className="landing-container__review">
-                <div className="landing-container__picture" />
-                <ul className="landing-container__item--hover">@Reviewer</ul>
-              </div>
-              <div className="landing-container__review">
-                <div className="landing-container__picture" />
-                <ul className="landing-container__item--hover">@Reviewer</ul>
-              </div>
+              {this.props.users.map((user, i) => {
+                if (i < 5) {
+                  return (
+                    // Need to write a component that shows all the userss by a certain user
+                    // <div key={users._id} onClick={() => this.props.useruserss(user)}>
+                    <div key={user._id}>
+                      <div className="landing-container__review">
+                        <div className="landing-container__picture" />
+                        <ul className="landing-container__item--hover">
+                          @{user.username}
+                        </ul>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
             </div>
           </div>
           <Modal
+            shouldCloseOnOverlayClick={false}
             isOpen={this.state.modalIsOpen}
             onRequestClose={this.closeModal}
             style={modalStyles}
@@ -151,21 +139,29 @@ class LandingPage extends Component {
                 <div className="modal-container">
                   <div className="modal__header">
                     <div className="header__title">
-                      {this.state.modalInfo.title}
+                      {this.state.modalInfo.newMongoId.name}
                     </div>
                     <div className="header__reviewer">
-                      {this.state.modalInfo.reviewer}
+                      @{this.state.modalInfo.reviewer.username}
                     </div>
                   </div>
                   <div className="modal__body">
-                    <div className="body__image">
-                      {this.state.modalInfo.image}
-                    </div>
+                    <div className="body__image">Yup</div>
                     <div className="body__stars">
-                      {this.state.modalInfo.stars}
+                      {" "}
+                      <StarRatings
+                        starDimension="20px"
+                        starSpacing="5px"
+                        rating={this.state.modalInfo.stars}
+                        starRatedColor="gold"
+                        starEmptyColor="grey"
+                        numberOfStars={5}
+                        name="rating"
+                      />
                     </div>
+                    <div>{this.state.modalInfo.title}</div>
                     <div className="body__review">
-                      {this.state.modalInfo.review}
+                      {this.state.modalInfo.body}
                     </div>
                   </div>
                   <div className="modal__footer">
@@ -180,11 +176,6 @@ class LandingPage extends Component {
               ) : null}
             </div>
           </Modal>
-        </div>
-        <div>
-          <Dropzone onDrop={this.handleDrop} multiple accept="image/*">
-            <p>Drop your files or click here to upload</p>
-          </Dropzone>
         </div>
       </div>
     );
