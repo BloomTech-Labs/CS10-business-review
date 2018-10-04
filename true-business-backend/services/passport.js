40661a5a8ba47fb6fed26a5ea68f13df9afbfc7c
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-10-04 14:02:22 -0700

    Heroku...

--- true-business-backend/services/passport.js
+++ true-business-backend/services/passport.js
@@ -16,8 +16,8 @@ passport.use(
   new GoogleStrategy(
     {
       callbackURL: "/auth/google/redirect",
-      clientID: process.env.REACT_APP_GOOGLEAUTHCLIENTID || process.env.googleClientID,
-      clientSecret: process.env.REACT_APP_GOOGLEAUTHSECRET || process.env.googleClientSecret
+      clientID: process.env.googleClientID || process.env.REACT_APP_GOOGLEAUTHCLIENTID,
+      clientSecret: process.env.googleClientSecret || process.env.REACT_APP_GOOGLEAUTHSECRET,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({
